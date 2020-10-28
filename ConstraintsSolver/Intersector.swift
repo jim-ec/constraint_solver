6ccb0caffe85506d8d2848c73d881add96f5497e
Jim Eckerlein <jim.eckerlein@gmail.com>
2020-10-28 18:45:44 +0100

    Intersect cube with the ground plane

---
+++ ConstraintsSolver/Intersector.swift
@@ -0,0 +1,45 @@
+//
+//  Intersector.swift
+//  ConstraintsSolver
+//
+//  Created by Jim Eckerlein on 28.10.20.
+//
+
+import Foundation
+
+struct Contact {
+    let penetration: simd_float3
+}
+
+/// A cube located at the origin, extending in the positive axis directions.
+struct Cube {
+    let sideLength: Float
+    let transform: Transform
+}
+
+/// Intersects a cube with the plane defined by `z = 0`, returning the penetration vector.
+func intersectCubeWithGround(cube: Cube) -> Contact {
+    let canonicalVertices: [simd_float3] = [
+        cube.sideLength * .zero,
+        cube.sideLength * .e1,
+        cube.sideLength * .e2,
+        cube.sideLength * .e1 + .e2,
+        cube.sideLength * .e3,
+        cube.sideLength * .e3 + .e1,
+        cube.sideLength * .e3 + .e2,
+        cube.sideLength * .e3 + .e1 + .e2
+    ]
+    
+    let vertices = canonicalVertices.map(cube.transform.apply)
+    
+    let deepestVertex = vertices.reduce(simd_float3(0, 0, .infinity)) { (reduction, vertex) in
+        if vertex.z < reduction.z {
+            return vertex
+        }
+        else {
+            return reduction
+        }
+    }
+    
+    return Contact(penetration: simd_float3(0, 0, deepestVertex.z))
+}
