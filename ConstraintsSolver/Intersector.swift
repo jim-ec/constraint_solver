14c890d52fe7b8e5c542b1d412dfb127be1c995a
Jim Eckerlein <jim.eckerlein@gmail.com>
2020-10-29 18:51:42 +0100

    Use general cuboid

--- ConstraintsSolver/Intersector.swift
+++ ConstraintsSolver/Intersector.swift
@@ -12,32 +12,36 @@ struct Contact {
     let penetratingVertex: simd_float3
 }
 
-/// A cube located at the origin, extending in the positive axis directions.
-struct Cube {
+/// A cuboid located at the origin, extending in the positive axis directions.
+struct Cuboid {
     let mass: Float
-    let sideLength: Float
+    let extent: simd_float3
     let transform: Transform
     
     func restTransform() -> Transform {
-        .translation(simd_float3(-sideLength, -sideLength, -sideLength))
+        .translation(simd_float3(-extent.x, -extent.y, -extent.z))
     }
     
     func inertiaTensor() -> simd_float3 {
-        simd_float3(repeating: 1.0 / 6.0 * mass * sideLength * sideLength)
+        1.0 / 12.0 * mass * simd_float3(
+            extent.y * extent.y + extent.z * extent.z,
+            extent.x * extent.x + extent.z * extent.z,
+            extent.x * extent.x + extent.y * extent.y
+        )
     }
 }
 
 /// Intersects a cube with the plane defined by `z = 0`, returning the penetration vector.
-func intersectCubeWithGround(cube: Cube) -> Contact {
+func intersectCuboidWithGround(cube: Cuboid) -> Contact {
     let canonicalVertices: [simd_float3] = [
-        cube.sideLength * .zero,
-        cube.sideLength * .e1,
-        cube.sideLength * .e2,
-        cube.sideLength * .e1 + .e2,
-        cube.sideLength * .e3,
-        cube.sideLength * .e3 + .e1,
-        cube.sideLength * .e3 + .e2,
-        cube.sideLength * .e3 + .e1 + .e2
+        simd_float3(0, 0, 0),
+        simd_float3(cube.extent.x, 0, 0),
+        simd_float3(0, cube.extent.y, 0),
+        simd_float3(cube.extent.x, cube.extent.y, 0),
+        simd_float3(0, 0, cube.extent.z),
+        simd_float3(cube.extent.x, 0, cube.extent.z),
+        simd_float3(0, cube.extent.y, cube.extent.z),
+        simd_float3(cube.extent.x, cube.extent.y, cube.extent.z)
     ]
     
     let vertices = canonicalVertices.map(cube.transform.act)
