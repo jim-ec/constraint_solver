9ced0cf579a0bccc1b47fe62cfdc555465404068
Jim Eckerlein <jim.eckerlein@icloud.com>
2022-09-11 22:20:32 +0200

    Setup

--- src/world.rs
+++ src/world.rs
@@ -51,14 +51,12 @@ impl World {
         )]);
 
         let mut rigid = rigid::Rigid::new(1.0);
-        rigid.external_force.z = -3.0;
-        // rigid.velocity.z = -0.2;
-        // rigid.frame.position.z = 2.0;
-        // rigid.frame.quaternion =
-        //     Quaternion::from_axis_angle(Vector3::new(1.0, 0.5, 0.2).normalize(), Rad(1.0));
-        rigid.frame.quaternion = Quaternion::from_angle_z(Rad(1.0));
-        rigid.frame.position.z = 1.0;
-        rigid.internal_torque.x = 1.5;
+        rigid.external_force.z = -5.0;
+        rigid.velocity.z = -0.2;
+        rigid.angular_velocity.z = 1.0;
+        rigid.frame.position.z = 5.0;
+        rigid.frame.quaternion =
+            Quaternion::from_axis_angle(Vector3::new(1.0, 0.5, 0.2).normalize(), Rad(1.0));
         rigid.past_frame = rigid.frame;
 
         World {
