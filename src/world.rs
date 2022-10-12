097910d2387933b6aa2e8659bdfa562d4a920b6e
Jim Eckerlein <jim.eckerlein@icloud.com>
2022-10-12 17:22:38 +0200

    Setup

--- src/world.rs
+++ src/world.rs
@@ -18,12 +18,10 @@ impl World {
 
         let mut a = rigid::Rigid::new(rigid_metrics);
 
-        // a.external_force.z = -5.0;
-        // a.velocity.z = -0.2;
+        a.external_force.z = -5.0;
+        a.velocity.z = -0.2;
         a.angular_velocity.x = 1.0;
-        // a.angular_velocity.y = 0.5;
-        a.frame.position.z = 2.0;
-        a.center_of_mass.y = 0.5;
+        a.frame.position = vec3(0.5, 0.5, 0.5);
 
         World { a }
     }
